dc72d7b04e0b119c8d39f28608a45fe46e9e89a5
Naohiro Hamada <nao.pinguinus@icloud.com>
2020-08-01 22:15:18 +0900

    Create Base/UI projects

---
+++ Base/Sources/MET_Base.h
@@ -0,0 +1,19 @@
+//
+//  MET_Base.h
+//  MET Base
+//
+//  Created by Naohiro Hamada on 2020/08/01.
+//  Copyright © 2020 Naohiro Hamada. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MET_Base.
+FOUNDATION_EXPORT double MET_BaseVersionNumber;
+
+//! Project version string for MET_Base.
+FOUNDATION_EXPORT const unsigned char MET_BaseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MET_Base/PublicHeader.h>
+
+
---
+++ MUI/MuseumUI/Sources/MuseumUI.h
@@ -0,0 +1,19 @@
+//
+//  MuseumUI.h
+//  MuseumUI
+//
+//  Created by Naohiro Hamada on 2020/08/01.
+//  Copyright © 2020 Naohiro Hamada. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MuseumUI.
+FOUNDATION_EXPORT double MuseumUIVersionNumber;
+
+//! Project version string for MuseumUI.
+FOUNDATION_EXPORT const unsigned char MuseumUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MuseumUI/PublicHeader.h>
+
+
